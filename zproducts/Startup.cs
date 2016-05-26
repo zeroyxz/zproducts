f2472dcc4688d825c2419e03f00c0609f4f56938
zeroyxz <peterworlin@hotmail.com>
2016-05-26 16:12:54 +0100

    New

---
+++ zproducts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(zproducts.Startup))]
+namespace zproducts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
